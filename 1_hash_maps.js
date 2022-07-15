a5851f577a25743702cd368d070df69691fa0948
Ladrillo <gabriel@cabrejas.net>
2022-07-15 19:03:31 +0200

    initial

---
+++ 1_hash_maps.js
@@ -0,0 +1,16 @@
+const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+function alphabet(n) {
+  // your code here
+}
+
+// Build a map to lookup the first n lowercase letters of the alphabet. The return value of alphabet() should be a map where the keys are numbers 0-25
+
+// Note: there are no performance gains to be gained from this kind of approach in the real world...it is just intended for practice working with the map data structure :)
+
+// Test cases:
+const lowercase = alphabet(26)
+console.log(lowercase.get(9)) // j
+
+const both_cases = alphabet(52)
+console.log(both_cases.get(51)) // Z
---
+++ README.md
@@ -0,0 +1,10 @@
+# Code-Along 12.1 - Big O Analysis and Caching
+
+## Requirements
+
+- Node >= 16.x (prefer current LTS version)
+
+## Set Up
+
+1. Clone this repository.
+2. Execute the scripts using Node. Example: `node 1_hash_maps.js`.
