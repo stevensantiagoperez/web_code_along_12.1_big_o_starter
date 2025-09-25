6dc5166183a8904184005d878f7cbe30a000c539
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-09-25 17:43:34 -0400

    done

--- 1_hash_maps.js
+++ 1_hash_maps.js
@@ -2,11 +2,19 @@ const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 function alphabet(n) {
   // your code here
+  const lookup = new Map
+  for (let i = 0; i < n; i++){
+    lookup.set(i, letters[i])
+  }
+  return lookup
 }
 
-// Build a map to lookup the first n lowercase letters of the alphabet. The return value of alphabet() should be a map where the keys are numbers 0-25
+// Build a map to lookup the first n lowercase letters of the alphabet.
+//  The return value of alphabet() should be a map where the keys are numbers 0-25
 
-// Note: there are no performance gains to be gained from this kind of approach in the real world...it is just intended for practice working with the map data structure :)
+// Note: there are no performance gains to be gained from this kind of approach
+//  in the real world...it is just intended for practice working with the map 
+// data structure :)
 
 // Test cases:
 const lowercase = alphabet(26)
--- 2_memoization.js
+++ 2_memoization.js
@@ -1,22 +1,45 @@
 function fibonacci(n) {
   if (n <= 0) return 0
   if (n == 1) return 1
-  return fibonacci(n - 1) + fibonacci(n - 2)
+
+  const memo = [0, 1]; // memo[0] = 0, memo[1] = 1
+
+  for (let i = 2; i <= n; i++) {
+    memo[i] = memo[i - 1] + memo[i - 2];
+  }
+
+  return memo[n];
 }
 
-// The starter code is a naive recursive solution for the computing the nth number in the Fibonacci sequence.
+// The starter code is a naive recursive solution for the computing the 
+// nth number in the Fibonacci sequence.
+
 // Runtime complexity: O(2^n)
 // Space complexity: O(n)
 
-// This is a very commonly used example of repeated computations that can (and should!) be optimized with memoization, and the runtime problem is best illustrated by a tree diagram of fibonacci() calls. Google "recursive fibonacci diagram" in case you haven't seen that visual before.
+// This is a very commonly used example of repeated computations that can
+//  (and should!) be optimized with memoization, and the runtime problem 
+// is best illustrated by a tree diagram of fibonacci() calls. Google 
+// "recursive fibonacci diagram" in case you haven't seen that visual before.
 
-// The total number of calls to the fibonacci function grows exponentially as n increases, causing a major runtime issue that will grind things to a halt, even before n gets that large.
+// The total number of calls to the fibonacci function grows exponentially 
+// as n increases, causing a major runtime issue that will grind things to 
+// a halt, even before n gets that large.
 
-// The easy fix here is to "remember the answer" to problems we've already solved, and this is called memoization. By simply keeping a map of all the fibonacci values we've already calculated, then we'll avoid having to repeat that work again later on.
+// The easy fix here is to "remember the answer" to problems we've already
+//  solved, and this is called memoization. By simply keeping a map of all 
+// the fibonacci values we've already calculated, then we'll avoid having 
+// to repeat that work again later on.
 
-// Specifically, the natural way to use a map (hash map) data structure here is as a kind of lookup table. After finding a solution for a given n, store a key/value pair where the key is n and the value is fibonacci(n). Then when you need that solution to build up to a higher level in the sequence, look it up in the map (a constant time operation!) rather than repeating all the work again.
+// Specifically, the natural way to use a map (hash map) data structure here 
+// is as a kind of lookup table. After finding a solution for a given n, 
+// store a key/value pair where the key is n and the value is fibonacci(n). 
+// Then when you need that solution to build up to a higher level in the 
+// sequence, look it up in the map (a constant time operation!) rather 
+// than repeating all the work again.
 
-// Optimize this naive solution to implement this memoization approach, for a Fibonacci implementaiton with linear O(n) (rather than exponential) runtime.
+// Optimize this naive solution to implement this memoization approach, 
+// for a Fibonacci implementaiton with linear O(n) (rather than exponential) runtime.
 
 // Test cases:
 console.log(fibonacci(0)) // 0
@@ -24,6 +47,8 @@ console.log(fibonacci(1)) // 1
 console.log(fibonacci(2)) // 1
 console.log(fibonacci(8)) // 21
 console.log(fibonacci(17)) // 1597
-// console.log(fibonacci(58)) // 591286729879
+console.log(fibonacci(58)) // 591286729879
 
-// Note: the final test case is commented out since it will blow up on even a very fast computer. With the memoized solution though, this test case will also run easily.
+// Note: the final test case is commented out since it will blow up on even 
+// a very fast computer. With the memoized solution though, this test case 
+// will also run easily.
